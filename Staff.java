6b9319562d7f7ecfa79db3da147a407bdf974437
JnSrzGTHB <pulidomarkjustine@gmail.com>
2025-04-07 18:21:22 +0800

    Update Staff.java

--- Staff.java
+++ Staff.java
@@ -1,18 +1,18 @@
-// Subclass: Staff (extends Person)
+
 public class Staff extends Person {
-    // Additional field for Staff
+  
     private String department;
 
-    // Constructor that accepts name, id, and department
+ 
     public Staff(String name, int id, String department) {
-        super(name, id); // Call parent constructor
+        super(name, id); 
         this.department = department;
     }
 
-    // Override displayInfo() to include department
+
     @Override
     public void displayInfo() {
-        super.displayInfo(); // Call parent method
+        super.displayInfo(); 
         System.out.println("Department: " + department);
     }
 }
