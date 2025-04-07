73b87de12a2bb509b560d5c1d9043d6ca8ca1901
JnSrzGTHB <pulidomarkjustine@gmail.com>
2025-04-07 18:23:34 +0800

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,15 +1,15 @@
-// Subclass: Teacher (extends Person)
+
 public class Teacher extends Person {
-    // Additional field for Teacher
+    
     private String subject;
 
-    // Constructor that accepts name, id, and subject
+   
     public Teacher(String name, int id, String subject) {
-        super(name, id); // Call parent constructor
+        super(name, id); 
         this.subject = subject;
     }
 
-    // Override displayInfo() to include subject
+  
     @Override
     public void displayInfo() {
         super.displayInfo(); // Call parent method
