246c9ad3a54db68d68baedb93c73e55734c5feb8
JnSrzGTHB <pulidomarkjustine@gmail.com>
2025-04-07 18:23:04 +0800

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,18 +1,18 @@
-// Subclass: Student (extends Person)
+
 public class Student extends Person {
-    // Additional field for Student
+    
     private String gradeLevel;
 
-    // Constructor that accepts name, id, and gradeLevel
+
     public Student(String name, int id, String gradeLevel) {
-        super(name, id); // Call parent constructor
+        super(name, id); 
         this.gradeLevel = gradeLevel;
     }
 
-    // Override displayInfo() to include gradeLevel
+ 
     @Override
     public void displayInfo() {
-        super.displayInfo(); // Call parent method
+        super.displayInfo(); 
         System.out.println("Grade Level: " + gradeLevel);
     }
 }
